b60fe9b0fd5cc365681e6dc21a0941c241075cf6
YaraPaes <yarapaesoliveira@gmail.com>
2023-06-06 17:18:33 -0300

    modal pages info finalizados

--- script/modal.js
+++ script/modal.js
@@ -1,51 +1,30 @@
-
-var modal = document.getElementById("ModalIDl");
-
-// Get the buttons that open the modal
-var artigos = document.getElementsByClassName("topicos__artigos");
-
-// Get the <button> element that closes the modal
 var btn = document.querySelector(".fechar");
 
-// When the user clicks on an artigo, open the modal
-for (var i = 0; i < artigos.length; i++) {
-  artigos[i].onclick = function() {
-    modal.style.display = "block";
-  };
-}
-
-// When the user clicks on the button (x), close the modal
 btn.onclick = function() {
   modal.style.display = "none";
-};
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-};
-
-function modalBlock() {
-    document.getElementById("ModalIDl").style.display="block";
-    setInterval(iniciarExercicio(), 100)
 }
 
-const contentPagesInfo = {
-    ansiedade: ansiedadeData,
-    depressao: depressaoData,
-    tdah: tdahData,
-}
-
-function modalInfo(page) {
+function modalInfo() {
     modal = document.getElementById("ModalIDl");
     title = document.getElementById("title");
     conteudo = document.getElementById("txt");
-    title.innerHTML = contentPagesInfo[page].info.title;
-    conteudo.innerHTML = contentPagesInfo[page].info.text;
+    title.innerHTML = contentPageInfo.info.title;
+    conteudo.innerHTML = contentPageInfo.info.text;
     modal.style.display = "block"
 }
 
-function modalArtic(page, artic) {
-    
+function modalArtic(articId) {
+    modal = document.getElementById("ModalIDl");
+    title = document.getElementById("title");
+    conteudo = document.getElementById("txt");
+    title.innerHTML = contentPageInfo.artics.find(item => item.id === articId).title;
+    conteudo.innerHTML = contentPageInfo.artics.find(item => item.id === articId).text;
+    modal.style.display = "block"
 }
